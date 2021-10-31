8377e77a8ab8261e42c3bcfd2878dc2b965e5a59
Christopher Lee <chris@codestorming.com>
2021-10-31 13:58:42 -0400

    fixup homepage to start

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
-import CardContainer from "../components/CardContainer";
-
 export default function Home() {
-  return <CardContainer id={1} />;
+  return (
+    <div className="m-4">
+      <h1 className="font-bold text-2xl mb-4">Tutorial Start</h1>
+      <p>
+        Welcome to my sample project for Modern Test Driven Development with:
+      </p>
+      <ul className="list-disc list-inside">
+        <li>Storybook</li>
+        <li>React Testing Library</li>
+        <li>Mock Service Worker</li>
+      </ul>
+    </div>
+  );
 }
