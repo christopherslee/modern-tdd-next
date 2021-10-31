ff610df7d06ffcdc4fc93f94d0f9bcb9cd16f567
Christopher Lee <chris@codestorming.com>
2021-10-31 14:05:06 -0400

    sample code and styles to get things moving

---
+++ samples/mockup.jsx
@@ -0,0 +1,22 @@
+<div className="rounded border shadow py-2 px-4 m-2 w-full max-w-xs inline-block">
+  <h1 className="text-gray-700 my-2 font-semibold">This is the title</h1>
+  <img src="" className="rounded w-full h-40" alt="some picture" />
+  <ul className="text-gray-400 mt-2">
+    <li className="inline-block cursor-pointer">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+        />
+      </svg>
+    </li>
+  </ul>
+</div>
