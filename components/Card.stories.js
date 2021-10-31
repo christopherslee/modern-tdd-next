9a763c268e5f4fe235b1b043a759397e1a3219e1
Christopher Lee <chris@codestorming.com>
2021-10-31 14:16:51 -0400

    create card and write storybook stories

--- components/Card.stories.js
+++ components/Card.stories.js
@@ -12,4 +12,27 @@ export default config;
 const Template = (args) => <Card {...args} />;
 
 export const Primary = Template.bind({});
-Primary.args = { id: 1 };
+Primary.args = {
+  id: 1,
+  title: "Hello EPIC Week",
+  imageUrl: "https://picsum.photos/id/1/300/200",
+  isFavorite: false,
+};
+
+export const LongText = Template.bind({});
+LongText.args = {
+  ...Primary.args,
+  title: "Hello EPIC Week I hope you had a great weekend",
+};
+
+export const Favorite = Template.bind({});
+Favorite.args = {
+  ...Primary.args,
+  isFavorite: true,
+};
+
+export const NotFavorite = Template.bind({});
+NotFavorite.args = {
+  ...Primary.args,
+  isFavorite: false,
+};
