8bea8f814a5903890f2ab41fee953e7e4739b7eb
Christopher Lee <chris@codestorming.com>
2021-10-31 20:14:59 -0400

    use card on page

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,11 @@
+import CardContainer from "../components/CardContainer";
+
 export default function Home() {
   return (
     <div className="m-4">
-      <h1 className="font-bold text-2xl mb-4">Tutorial Start</h1>
-      <p>
-        Welcome to my sample project for Modern Test Driven Development with:
-      </p>
-      <ul className="list-disc list-inside">
-        <li>Storybook</li>
-        <li>React Testing Library</li>
-        <li>Mock Service Worker</li>
-      </ul>
+      <CardContainer id={1} />
+      <CardContainer id={2} />
+      <CardContainer id={3} />
     </div>
   );
 }
